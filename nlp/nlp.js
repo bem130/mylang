08632f068f0127c78db625dc257deee1340a0994
bem130 <haruki070617@gmail.com>
2023-01-05 20:47:59 +0900

    make a little

--- nlp/nlp.js
+++ nlp/nlp.js
@@ -15,37 +15,62 @@ class NLPparse {
             i++;
         }
     }
+    error(i,msg) {
+        console.error(`[error:${i}]`,...msg)
+    }
+    info(msg) {
+        console.log(`[info]`,...msg)
+    }
     toplevel() {
         // <code> ::= { <blank-lines> <func> <blank-lines> }
         let i = 0;
         while (i<this.code.length) {
-            // <func> ::= '!' [ <space> ] 'fn:' <func-name> '(' <func-def-arg> ')' [ <space> ] '{' <block> '}'
+            // <func> ::= '!' [ <space> ] <var-type> ':fn:' [ <space> ] <func-name> '(' <func-arg-def> ')' [ <space> ] '{' <block> '}'
             if (this.code[i]=='!') { // '!'
+                let func = {
+                    name: "",
+                    args: "",
+                    return: "",
+                    block: "",
+                };
                 // [ <space> ]
                 i++;
                 while (i<this.code.length&&this.code[i]==" ") {i++;}
+                // <var-type> ':'
+                while (i<this.code.length&&this.code[i]!=":") {
+                    func.return += this.code[i];
+                    i++;
+                }
                 // 'fn:'
-                if (this.code.startsWith('fn:',i)) {
-                    i+=3;
-                    let func = {name:""};
-                    // <func-name> '('
-                    while (i<this.code.length&&this.code[i]!="(") {
-                        func.name += this.code[i];
-                        i++;
-                    }
-                    console.log(func.name);
+                i++;
+                if (!this.code.startsWith('fn:',i)) {
+                    this.error(i,["関数の定義に問題があります"]);
+                    return false;
                 }
-                else {
-                    console.error("fn:がありません");
+                i+=3;
+                // [ <space> ]
+                i++;
+                while (i<this.code.length&&this.code[i]==" ") {i++;}
+                // <func-name> '('
+                while (i<this.code.length&&this.code[i]!="(") {
+                    func.name += this.code[i];
+                    i++;
                 }
+                // <func-arg-def> ')'
+                i++;
+                while (i<this.code.length&&this.code[i]!=")") {
+                    func.args += this.code[i];
+                    i++;
+                }
+                this.info(["関数を読み込みました",func]);
             }
             else if (this.code[i]==" ") {
             }
             else if (this.code[i]=="\n"|(this.code[i]=="\r"&&this.code[i+1]=="\n")|this.code[i]=="\0") {
             }
             else {
-                console.log(this.code[i])
-                console.error("トップレベルに関数、改行、空白以外が存在します");
+                this.error(i,["トップレベルに関数、改行、空白以外が存在します'",this.code[i],"'"]);
+                return false;
             }
             i++;
         }
@@ -56,9 +81,9 @@ class NLPparse {
 
 {
     let testcode = `
-        !fn:main(
-        ! fn:sub(
+        !void:fn:main()
+        ! int:fn: sub (int:x)
+        ! int:fn: add (int:x,int:y)
     `
     new NLPparse(testcode);
 }
-console.log("hello")
